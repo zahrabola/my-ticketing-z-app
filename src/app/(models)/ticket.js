fc048a8962880e29b4221694afe10c51c3ed1ec2
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-12-09 20:00:19 +0000

    update

---
+++ src/app/(models)/ticket.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.Promise = global.Promise
+
+const ticketSchema = new Schema (
+{
+ title: String,
+ description: String, 
+ categoru: String, 
+ priority: Number, 
+ progress:Number,
+ status: String,
+ active: Boolean,
+},
+{
+    timestamps: true,
+
+}
+)
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,3 +15,5 @@ const Dashboard = () => {
 }
 
 export default   Dashboard;
+
+
