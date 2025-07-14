2123ebb8dc9acd14920299dbf6a2cfea37506f26
zahrabola <95353397+zahrabola@users.noreply.github.com>
2025-07-14 20:17:33 +0100

    update
    fetch ticket data

--- src/app/api/Tickets/[id]/route.js
+++ src/app/api/Tickets/[id]/route.js
@@ -1,6 +1,26 @@
 import  Ticket from "@/app/(models)/ticket";
 import { NextResponse } from "next/server";
 
+export async function GET (req, {params}){
+  
+  try {
+  const { id } = params;
+  const foundTicket = await Ticket.findOne({ _id: id});
+  return NextResponse.json({ foundTicket}, {status: 200});
+
+  } catch (error) {
+  console.log(error);
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+
+ 
+
+}
+
+
+
+
+
 export async function DELETE(req, { params }) {
   try {
     const { id } = params;
--- src/app/ticketpage/[id]/page.jsx
+++ src/app/ticketpage/[id]/page.jsx
@@ -1,5 +1,19 @@
-import React from 'react'
+import React, { cache } from 'react'
 import TicketForm from '@/app/(components)/TicketForm'
+
+const getTicketById = async(id) => {
+
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+     cache:"no-store",
+    });
+    
+   if(!res.ok){
+    throw new Error("Failed to get ticket.")
+  }
+
+return res.json();
+
+}
 
 
 const Ticketpage = async ({params }) => {
@@ -10,11 +24,17 @@ const Ticketpage = async ({params }) => {
       if(EDITMODE) {
         updateTicketData = await getTicketById(params.id);
         console.log(updateTicketData)
+        updateTicketData= updateTicketData.foundTicket;
+
+      } else {
+        updateTicketDatam= {
+          _id:"new"
+        }
       }
   return (
-
-    <div>Ticketpage {params.id}
-     <TicketForm />
+ ///{params.id}
+    <div>Ticketpage
+     <TicketForm ticket={updateTicketData} />
     </div>
    
   )
